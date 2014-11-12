3cb93171967b6c793581b5cf80e7176d0cdd1556
Joao Carreira <joao@berkeley.edu>
2014-11-12 15:45:50 -0800

    Refactoring of get_data_files

--- scripts/nodes_metrics/get_data_files.pl
+++ scripts/nodes_metrics/get_data_files.pl
@@ -1,14 +1,19 @@
 #!/usr/bin/perl
 
+# get nodes utilization from the firebox cluster
+# plot this data in graphs
+# generate pdf
+
 use strict;
 use warnings;
 
 my $NODES = 16;
 my $USER = 'joao';
+my $FIREBOX_URL = 'fbox.millennium.berkeley.edu';
 
 for my $i (1..$NODES) {
 	print "Getting file from f$i\n";
-	`scp $USER\@fbox.millennium.berkeley.edu:~/dstat_data/out_f$i data/data_original/`;
+	`scp $USER\@$FIREBOX_URL:~/dstat_data/out_f$i data/data_original/`;
 	`tail -n+7 data/data_original/out_f$i > data/data_original/metrics_$i`;
 	`Rscript plot.r $i data/data_original/metrics_$i data/data_results/plot_f$i.pdf`;
 	`rm data/data_original/out_f$i`;
@@ -17,5 +22,4 @@ for my $i (1..$NODES) {
 my $cmd = "pdftk data/data_results/plot*.pdf cat output - > data/data_results/nodes_usage.pdf";
 print "$cmd\n";
 `$cmd`;
-
 
